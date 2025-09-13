7196186362ed4e62ab94e425e9e36e24eba0a3df
shidaoqingren4-source <shidaoqingren4@gmail.com>
2025-09-13 15:11:41 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+window.addEventListener("DOMContentLoaded", () => {
 console.log("loggedIn:", localStorage.getItem("loggedIn"));
 console.log("userName:", localStorage.getItem("userName"));
 
@@ -273,6 +274,7 @@ document.getElementById("prefSelect").addEventListener("change", function () {
 
   cityContainer.style.display = "block";
 });
+};
 
 function search() {
   const Input = document.getElementById("cityInput")?.value.trim() || "";
@@ -330,6 +332,7 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
 
 
 
