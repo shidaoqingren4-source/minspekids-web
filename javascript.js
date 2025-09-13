c9c82aec720f1b1b23808a105e558bb7e7bc954c
shidaoqingren4-source <shidaoqingren4@gmail.com>
2025-09-13 16:45:07 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -306,29 +306,7 @@ document.getElementById("cityInput").addEventListener("keydown", function (event
 
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-  const loginFigure = document.getElementById("login");
-  const loginIcon = document.getElementById("login-icon");
-  const navText = loginFigure?.querySelector("figcaption nav");
 
-  console.log("loggedIn:", localStorage.getItem("loggedIn"));
-  console.log("navText:", navText);
-
-  if (localStorage.getItem("loggedIn") === "true") {
-    if (navText) {
-      navText.textContent = "マイページ";
-    }
-    if (loginIcon) {
-      loginIcon.classList.remove("fa-user");
-      loginIcon.classList.add("fa-id-card");
-    }
-
-    loginFigure?.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.location.href = "mypage.html";
-    });
-  }
-});
 
 
 
